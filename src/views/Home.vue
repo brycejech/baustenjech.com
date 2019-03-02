216762634ab51ad62a33cf4ed95455a04ec58901
Bryce Jech <bryce@brycejech.com>
2019-03-02 13:11:08 -0600

    add Poem to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <section id="home">
+        <Poem />
+
+    </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+'use strict';
+
+import Poem from '@/components/Poem';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    components: {
+        Poem
+    }
 }
 </script>
+
+<style lang="sass" scoped>
+
+</style>
