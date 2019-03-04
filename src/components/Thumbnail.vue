67e28d81dbd1c6bafd60008f14f5f47f46beb71e
Bryce Jech <bryce@brycejech.com>
2019-03-04 07:32:24 -0600

    add Thumbnail component

---
+++ src/components/Thumbnail.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+    <div class="thumbnail">
+        <img :src="thumb" :alt="alt">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Thumnail',
+    props: [ 'thumb', 'image', 'alt' ],
+    methods: {
+        getImage(path){
+            return require(path);
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
